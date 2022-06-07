f71eec50891e23bb5aa144f513307cdebf5fd953
Matthew Callens <callensmatt@gmail.com>
2022-06-07 11:44:34 -0400

    basic web server

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,24 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::web::Bytes;
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port: u16 = match option_env!("PORT") {
+        Some(p) => p.parse().unwrap(),
+        None => 8008,
+    };
+
+    HttpServer::new(|| App::new().service(rpc))
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
+}
+
+#[post("/")]
+async fn rpc(payload: Bytes) -> impl Responder {
+    dbg!(payload);
+    HttpResponse::Ok()
 }
